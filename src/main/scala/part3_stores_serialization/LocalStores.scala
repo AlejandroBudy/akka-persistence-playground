4508f37c1ef3b1817c9748fc83b868cbd0e04e39
Alejandro <atorresato@gmail.com>
2022-06-29 10:33:10 +0200

    Local levelDB lessons

---
+++ src/main/scala/part3_stores_serialization/LocalStores.scala
@@ -0,0 +1,51 @@
+package part3_stores_serialization
+
+import akka.actor.{ActorLogging, ActorSystem, Props}
+import akka.persistence.{PersistentActor, SaveSnapshotFailure, SaveSnapshotSuccess, SnapshotOffer}
+import com.typesafe.config.ConfigFactory
+
+object LocalStores extends App {
+
+  class SimplePersistentActor extends PersistentActor with ActorLogging {
+    var nMessages = 0
+
+    override def receiveRecover: Receive = {
+      case SnapshotOffer(_, payload: Int) =>
+        log.info(s"Recovered snapshot $payload")
+        nMessages = payload
+      case message =>
+        log.info(s"Recovered $message")
+        nMessages += 1
+    }
+
+    override def receiveCommand: Receive = {
+      case "print" => log.info(s"I have persisted $nMessages so far")
+      case "snap"  => saveSnapshot(nMessages)
+      case SaveSnapshotSuccess(_) =>
+        log.info(s"Save snapshot was successful")
+      case SaveSnapshotFailure(_, cause) =>
+        log.warning(s"Save snapshot failure $cause")
+      case message =>
+        persist(message) { e =>
+          log.info(s"Persisting $message")
+          nMessages += 1
+        }
+    }
+
+    override def persistenceId: String = "simple-persistent-actor"
+  }
+
+  val localStoresActorSystem =
+    ActorSystem("localStoresSystem", ConfigFactory.load().getConfig("localStores"))
+  val persistentActor = localStoresActorSystem.actorOf(Props[SimplePersistentActor])
+
+  for (i <- 1 to 10)
+    persistentActor ! s"i love Akka $i"
+
+  persistentActor ! "print"
+  persistentActor ! "snap"
+
+  for (i <- 11 to 20)
+    persistentActor ! s"i love Akka $i"
+
+}
